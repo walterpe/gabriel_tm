4b8223628134660982dcbb0263ba663f01451494
walterpe <walter.github.com@pinedaespinosa.org>
2021-06-05 10:58:46 +0200

    if conditional, comparison, modulo (even/odd)

--- my_main.cpp
+++ my_main.cpp
@@ -13,22 +13,30 @@ int main() {
     // arrays : collection of values 
     const int my_length = 3;
     int my_firsts[my_length]  = { 15, 12, 6 };
-    int my_seconds[my_length] = {  3,  4, 2 };
+    int my_seconds[my_length] = {  5,  3, 2 };
 
     
-    // iteration : until my_length
+    // iteration : until my_length.
     for (int i=0; i < my_length ; i++ ) {
+       
        // extract from array
        int my_first  = my_firsts[i];
        int my_second = my_seconds[i];
-        
-       // function calls
-       int my_prod = my_funct_mult (my_first, my_second);
-       int my_div  = myFunctDiv    (my_first, my_second);
+
+       if (  my_first % 2 == 0 ) { // is even ( % yields modulo of division )
+          cout << "iteration : " << i << " : even " << my_first << ", accepted . \n";
+          
+          // function calls
+          int my_prod = my_funct_mult (my_first, my_second);
+          int my_div  = myFunctDiv    (my_first, my_second);
+          
+          // show in console
+          cout << "prod " << my_first << "*" << my_second << " = " << my_prod << "\n";
+          cout << "div  " << my_first << "/" << my_second << " = " << my_div  << "\n";
+       } else {  // is odd
+          cout << "iteration : " << i << " : odd " << my_first << ", ignored . \n";
+       }
        
-       // show in console
-       cout << "prod " << my_first << "*" << my_second << " = " << my_prod << "\n";
-       cout << "div  " << my_first << "/" << my_second << " = " << my_div  << "\n";
        cout << "\n";
     }
 
